7eb17488ba1f03aee454bc6996327a5f2ca4c5f2
Gaurav <gauravburande2425@gmail.com>
2023-03-10 00:28:31 +0530

    Drawer Completed

--- components/Drawer.js
+++ components/Drawer.js
@@ -3,31 +3,63 @@ import React from 'react';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Feather from 'react-native-vector-icons/Feather';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 const Drawer = () => {
   return (
     <View style={styles.drawer}>
-      <View style={styles.close_container_div}>
-        <View style={styles.close_container}>
-          <AntDesign name="close" size={35} color="#FFFFFF"></AntDesign>
+      <View style={styles.drawer_upperpart}>
+        <View style={styles.close_container_div}>
+          <View style={styles.close_container}>
+            <AntDesign name="close" size={35} color="#FFFFFF"></AntDesign>
+          </View>
+        </View>
+
+        <View style={styles.new_chat}>
+          <Feather
+            name="plus-square"
+            size={30}
+            color="#000000"
+            style={styles.plus_icon}></Feather>
+          <Text style={styles.new_chat_txt}>New Chat</Text>
+        </View>
+
+        <View style={styles.history}>
+          <MaterialIcons
+            name="history"
+            size={30}
+            color="#FFFFFF"
+            style={styles.history_icon}></MaterialIcons>
+          <Text style={styles.history_txt}>History</Text>
         </View>
       </View>
 
-      <View style={styles.new_chat}>
-        <Feather
-          name="plus-square"
-          size={30}
-          color="#000000"
-          style={styles.plus_icon}></Feather>
-        <Text style={styles.new_chat_txt}>New Chat</Text>
-      </View>
+      <View style={styles.drawer_lowerpart}>
+        <View style={styles.history}>
+          <MaterialCommunityIcons
+            name="theme-light-dark"
+            size={30}
+            color="#FFFFFF"
+            style={styles.history_icon}></MaterialCommunityIcons>
+          <Text style={styles.history_txt}>Dark Mode</Text>
+        </View>
 
-      <View style={styles.history}>
-        <MaterialIcons
-          name="history"
-          size={30}
-          color="#FFFFFF"
-          style={styles.history_icon}></MaterialIcons>
-        <Text style={styles.history_txt}>History</Text>
+        <View style={styles.history}>
+          <Feather
+            name="info"
+            size={30}
+            color="#FFFFFF"
+            style={styles.history_icon}></Feather>
+          <Text style={styles.history_txt}>About Us</Text>
+        </View>
+
+        <View style={styles.history}>
+          <Feather
+            name="log-out"
+            size={30}
+            color="#FFFFFF"
+            style={styles.history_icon}></Feather>
+          <Text style={styles.history_txt}>Log Out</Text>
+        </View>
       </View>
     </View>
   );
@@ -50,9 +82,12 @@ const styles = StyleSheet.create({
     zIndex: 1111111,
     // justifyContent: 'center',
     alignItems: 'center',
+    justifyContent: 'space-between',
+    display: 'flex',
+    flexDirection: 'column',
   },
   close_container_div: {
-    width: P90,
+    width: P100,
     // backgroundColor: '#F65F65',
     marginTop: 10,
     display: 'flex',
@@ -67,7 +102,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     display: 'flex',
     borderRadius: 50,
-    marginRight: 10,
+    // marginRight: 10,
   },
   plus_icon: {
     marginLeft: 5,
@@ -81,7 +116,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'flex-start',
     alignItems: 'center',
-    width: P90,
+    width: P100,
     backgroundColor: '#FFFFFF',
     marginTop: 10,
     paddingTop: 5,
@@ -92,8 +127,8 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'flex-start',
     alignItems: 'center',
-    width: P90,
-    backgroundColor: '#242424',
+    width: P100,
+    // backgroundColor: '#242424',
     marginTop: 10,
     paddingTop: 5,
     paddingBottom: 5,
@@ -109,5 +144,16 @@ const styles = StyleSheet.create({
     fontFamily: 'Gilroy-Bold',
     fontSize: 17,
     marginLeft: 10,
+  },
+  drawer_upperpart: {
+    width: P90,
+    // backgroundColor: '#F65F65',
+  },
+  drawer_lowerpart: {
+    width: P90,
+    // backgroundColor: '#F65F65',
+    borderTopColor: '#FFFFFF',
+    borderTopWidth: 0.8,
+    marginBottom: 20,
   },
 });
